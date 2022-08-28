03b28db9ae56ab0bebc1943fceb59b72e14be29d
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-28 10:49:09 +0430

    Initial Repo

---
+++ InsertionSortInArray/InsertionSortInArray/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortInArray
+{
+    public class InsertionSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = -1;
+                int current = i + 1;
+                int compareTo = i;
+                for (int j = current; j > 0; j--)
+                {
+                    if (array[current] < array[compareTo])
+                    {
+                        minIndex = compareTo;
+                    }
+                    compareTo--;
+                }
+
+                if (minIndex != -1)
+                {
+                    int temp = array[current];
+                    for (int k = current; k > minIndex; k--)
+                    {
+                        array[k] = array[k - 1];
+                    }
+
+                    array[minIndex] = temp;
+                }
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ InsertionSortInArray/InsertionSortInArray/Program.cs
@@ -0,0 +1,7 @@
+﻿using InsertionSortInArray;
+
+int[] array = { 99, 34, 100000, -19, 444, 3, 3, 3, 3, 2, 0, -1 };
+
+InsertionSort sort = new InsertionSort();
+
+sort.Sort(array);
